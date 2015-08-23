b0f2cc60d9967996da9703d6988d82e60048963e
Sabeeh <mr.hussnain.it@gmail.com>
2015-08-23 10:44:11 -0700

    share action provider

--- app/src/main/java/com/suh/itboy/suhweather/DetailActivity.java
+++ app/src/main/java/com/suh/itboy/suhweather/DetailActivity.java
@@ -1,10 +1,8 @@
 package com.suh.itboy.suhweather;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -12,31 +10,8 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_detail, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        switch (id) {
-            case R.id.action_settings:
-                Intent settingsIntent = new Intent(DetailActivity.this, SettingsActivity.class);
-                startActivity(settingsIntent);
-                break;
-        }
-
-        return super.onOptionsItemSelected(item);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
 
--- app/src/main/java/com/suh/itboy/suhweather/DetailActivityFragment.java
+++ app/src/main/java/com/suh/itboy/suhweather/DetailActivityFragment.java
@@ -3,6 +3,8 @@ package com.suh.itboy.suhweather;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -15,7 +17,7 @@ import android.widget.TextView;
  * A placeholder fragment containing a simple view.
  */
 public class DetailActivityFragment extends Fragment {
-
+    String forecast;
     public DetailActivityFragment() {
     }
 
@@ -23,8 +25,9 @@ public class DetailActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        String forecast = getActivity().getIntent().getStringExtra(Intent.EXTRA_TEXT);
         View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+
+        forecast = getActivity().getIntent().getStringExtra(Intent.EXTRA_TEXT);
         TextView detail_text = (TextView) rootView.findViewById(R.id.detail_text);
         detail_text.setText(forecast);
 
@@ -35,6 +38,12 @@ public class DetailActivityFragment extends Fragment {
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         inflater.inflate(R.menu.menu_detail, menu);
+
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        ShareActionProvider actionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+        if (actionProvider != null) {
+            actionProvider.setShareIntent(shareForecastIntent());
+        }
     }
 
     @Override
@@ -42,8 +51,20 @@ public class DetailActivityFragment extends Fragment {
         int id = item.getItemId();
 
         switch (id) {
-
+            case R.id.action_settings:
+                Intent settingsIntent = new Intent(getActivity(), SettingsActivity.class);
+                startActivity(settingsIntent);
+                break;
         }
         return true;
+    }
+
+    private Intent shareForecastIntent() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, forecast);
+
+        return shareIntent;
     }
 }
